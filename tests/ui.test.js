3be71c1d6878c14d7d2da70e21bd53bc22bbd1d0
helinzamar <164181321+helinzamar@users.noreply.github.com>
2025-06-02 22:51:12 +0200

    Added Selenium test for initial stack value

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -1,12 +1,19 @@
-const { Builder, By, until } = require('selenium-webdriver');
+const { Builder, By } = require('selenium-webdriver');
+require('chromedriver');
 
-test('Google has correct title', async () => {
-  const driver = await new Builder().forBrowser('chrome').build();
-  try {
-    await driver.get('https://www.google.com');
-    const title = await driver.getTitle();
-    expect(title).toMatch(/Google/);
-  } finally {
-    await driver.quit();
-  }
-}, 20000); // timeout 20 sekunder
+let driver;
+jest.setTimeout(10000);
+
+beforeAll(async () => {
+  driver = await new Builder().forBrowser('chrome').build();
+  await driver.get('file://' + __dirname + '/../dist/index.html');
+});
+
+afterAll(async () => {
+  await driver.quit();
+});
+
+test('Page should load with initial text n/a', async () => {
+  let el = await driver.findElement(By.id('top_of_stack')).getText();
+  expect(el).toBe("n/a");
+});
